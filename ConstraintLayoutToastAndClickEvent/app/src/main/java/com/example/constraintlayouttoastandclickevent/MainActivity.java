f24de1aaddf6eff6e207fb0447b25970dd80ead5
Syed Muhammad Arsalan Shah <smarsalanshah125@gmail.com>
2021-12-14 18:17:13 +0500

    Android

--- ConstraintLayoutToastAndClickEvent/app/src/main/java/com/example/constraintlayouttoastandclickevent/MainActivity.java
+++ ConstraintLayoutToastAndClickEvent/app/src/main/java/com/example/constraintlayouttoastandclickevent/MainActivity.java
@@ -4,7 +4,6 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.text.Layout;
-import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,34 +11,32 @@ import android.widget.Button;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-    Button button;
-
+Button  button ;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        button = findViewById(R.id.button4);
+         button=findViewById(R.id.button4);
 
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
+         button.setOnClickListener(new View.OnClickListener() {
+             @Override
+             public void onClick(View view) {
+             LayoutInflater layoutInflater = getLayoutInflater();
+             View layout =layoutInflater.inflate(R.layout.activity_main2,(ViewGroup) findViewById(R.id.act));
 
-                Toast.makeText(MainActivity.this, "Account Successfully created", Toast.LENGTH_LONG).show();
-            }
-        });
+             Toast toast =new Toast(getApplicationContext());
+             toast.setView(layout);
+             toast.setDuration(Toast.LENGTH_SHORT);
+             toast.show();
+    //Toast.makeText(MainActivity.this, "Account Successfully created", Toast.LENGTH_LONG).show();
+             }
+         });
 
     }
 
 
     public void hamza(View view) {
-//        LayoutInflater layoutInflater = getLayoutInflater();
-//        View layout = layoutInflater.inflate(R.layout.activity_main2, (ViewGroup) findViewById(R.id.act));
-//
-//        Toast toast = new Toast(getApplicationContext());
-//        toast.setGravity(Gravity.CENTER_VERTICAL, 0, 0);
-//        toast.setDuration(Toast.LENGTH_SHORT);
-//        toast.setView(layout);
-//        toast.show();
+
         Toast.makeText(this, "Login Successfully  ", Toast.LENGTH_SHORT).show();
     }
 }
